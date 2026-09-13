175e45a06d8a07070d5352a2d6e203f19dfe84db
aryansingh99956-dotcom <aryansingh99956@gmail.com>
2026-09-13 19:51:59 +0530

    fix render port

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,38 @@
- require("dotenv").config({ path: "./.env"});
- console.log("MONGO_URI:", process.env.MONGO_URI);
- const app = require("./app");
- const connectToDB = require("./config/database");
- const interviewRoutes = require("./src/routes/interview.routes");
- app.use("/api/interview",interviewRoutes);
- connectToDB();
- 
+ require("dotenv").config({
+  path: "./.env",
+});
 
- app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-   });
+console.log(
+  "MONGO_URI:",
+  process.env.MONGO_URI ? "Loaded" : "Missing"
+);
+
+const app = require("./app");
+const connectToDB = require("./config/database");
+
+const interviewRoutes = require("./src/routes/interview.routes");
+
+
+// ==========================================
+// ROUTES
+// ==========================================
+
+app.use("/api/interview", interviewRoutes);
+
+
+// ==========================================
+// DATABASE
+// ==========================================
+
+connectToDB();
+
+
+// ==========================================
+// SERVER
+// ==========================================
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
